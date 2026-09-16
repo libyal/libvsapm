40183caa16124568d4479257febe055376431a46
Joachim Metz <joachim.metz@gmail.com>
2026-09-16 10:16:46 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libvsapm/.libs/libvsapm.1.dylib && test -f ./pyvsapm/.libs/pyvsapm.so
+if [ -f "${PWD}/libvsapm/.libs/libvsapm.1.dylib" ] && [ -f ./pyvsapm/.libs/pyvsapm.so ]
 then
-	install_name_tool -change /usr/local/lib/libvsapm.1.dylib ${PWD}/libvsapm/.libs/libvsapm.1.dylib ./pyvsapm/.libs/pyvsapm.so
+    install_name_tool -change /usr/local/lib/libvsapm.1.dylib "${PWD}/libvsapm/.libs/libvsapm.1.dylib" ./pyvsapm/.libs/pyvsapm.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
